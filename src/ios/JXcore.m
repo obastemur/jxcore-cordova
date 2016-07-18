a8dbb6951b5f1978df288232e16fe7fbe70928c2
Oguz Bastemur <obastemur@gmail.com>
2016-07-18 15:47:57 +0200

    update ios base file to new API

--- src/ios/JXcore.m
+++ src/ios/JXcore.m
@@ -32,13 +32,13 @@ SOFTWARE.
 {}
 - (JXcoreNative) getCallback;
 - (void) setCallback:(JXcoreNative)native;
-- (JXValue*) getFunction;
-- (void) setFunction:(JXValue*)fnc;
+- (JS_Value*) getFunction;
+- (void) setFunction:(JS_Value*)fnc;
 @end
 
 @implementation CPPWrapper {
   JXcoreNative native_;
-  JXValue jxvalue_;
+  JS_Value JS_Value_;
 }
 
 - (JXcoreNative) getCallback {
@@ -49,12 +49,12 @@ SOFTWARE.
   native_ = native;
 }
 
-- (JXValue*) getFunction {
-  return &jxvalue_;
+- (JS_Value*) getFunction {
+  return &JS_Value_;
 }
 
-- (void) setFunction:(JXValue*)fnc {
-  jxvalue_ = *fnc;
+- (void) setFunction:(JS_Value*)fnc {
+  JS_Value_ = *fnc;
 }
 @end
 
@@ -158,36 +158,36 @@ SOFTWARE.
 
 static NSMutableDictionary *natives;
 
-void ConvertParams(JXValue *results, int argc, NSMutableArray *params) {
+void ConvertParams(JS_Value *results, int argc, NSMutableArray *params) {
   for (int i=0; i<argc; i++) {
-    JXValue *result = results+i;
+    JS_Value *result = results+i;
     NSObject *objValue = nil;
 
     switch (result->type_) {
       case RT_Boolean: {
-        bool bl = JX_GetBoolean(result);
+        bool bl = JS_GetBoolean(result);
         JXBoolean *nmr = [[JXBoolean alloc] init];
         [nmr setBoolean:bl];
         objValue = nmr;
       } break;
       case RT_Int32: {
-        int nt = JX_GetInt32(result);
+        int nt = JS_GetInt32(result);
         objValue = [NSNumber numberWithInt:nt];
       } break;
       case RT_Double: {
-        double nt = JX_GetDouble(result);
+        double nt = JS_GetDouble(result);
         objValue = [NSNumber numberWithDouble:nt];
       } break;
       case RT_Buffer: {
-        char *data = JX_GetString(result);
-        int32_t len = JX_GetDataLength(result);
+        char *data = JS_GetString(result);
+        int32_t len = JS_GetDataLength(result);
         objValue =
             [NSData dataWithBytes:(const void *)data length:sizeof(char) * len];
         free(data);
       } break;
       case RT_Object: {
-        char *data = JX_GetString(result);
-        int ln = JX_GetDataLength(result);
+        char *data = JS_GetString(result);
+        int ln = JS_GetDataLength(result);
         if (ln > 0 && *data != '{' && *data != '[') {
           objValue = [NSString stringWithUTF8String:data];
         } else {
@@ -201,7 +201,7 @@ void ConvertParams(JXValue *results, int argc, NSMutableArray *params) {
       } break;
       case RT_Error:
       case RT_String: {
-        char *data = JX_GetString(result);
+        char *data = JS_GetString(result);
         objValue = [NSString stringWithUTF8String:data];
 
         free(data);
@@ -215,21 +215,19 @@ void ConvertParams(JXValue *results, int argc, NSMutableArray *params) {
   }
 }
 
-static void callback(JXValue *results, int argc) { }
-
-static void callJXcoreNative(JXValue *results, int argc) {
-  if (argc < 2 || !JX_IsString(results + 0) || !JX_IsString(results+(argc-1))) {
+static void callJXcoreNative(JS_Value *results, int argc) {
+  if (argc < 2 || !JS_IsString(results + 0) || !JS_IsString(results+(argc-1))) {
     NSLog(@"Unknown call received to callJXcoreNative. First and last parameters have to be string\n");
     return;
   }
   
   NSMutableArray *params = [[NSMutableArray alloc] init];
   
-  char *dt_name = JX_GetString(results + 0);
+  char *dt_name = JS_GetString(results + 0);
   NSString *name = [NSString stringWithUTF8String:dt_name];
   free(dt_name);
   
-  dt_name = JX_GetString(results + (argc-1));
+  dt_name = JS_GetString(results + (argc-1));
   NSString *callbackId = [NSString stringWithUTF8String:dt_name];
   free(dt_name);
   
@@ -257,18 +255,18 @@ static void callJXcoreNative(JXValue *results, int argc) {
   NSLog(@"Native method %@ not found.", name);
 }
 
-static void defineEventCB(JXValue *params, int argc) {
-  if (argc < 2 || !JX_IsFunction(params+1) || !JX_IsString(params+0)) {
+static void defineEventCB(JS_Value *params, int argc) {
+  if (argc < 2 || !JS_IsFunction(params+1) || !JS_IsString(params+0)) {
     NSLog(@"defineEventCB expects a function");
     return;
   }
 
   CPPWrapper *cpp = [[CPPWrapper alloc] init];
-  JXValue *fnc = params + 1;
-  JX_MakePersistent(fnc);
+  JS_Value *fnc = params + 1;
+  JS_MakePersistent(fnc);
   [cpp setFunction:fnc];
   
-  char *data = JX_GetString(params+0);
+  char *data = JS_GetString(params+0);
   NSString *name = [NSString stringWithUTF8String:data];
   free(data);
   
@@ -328,20 +326,20 @@ static float delay = 0;
       [[NSBundle mainBundle] pathForResource:fileName ofType:@"js"];
 
   [JXcore addNativeMethod:jxCallback withName:name];
-  
+
   NSError *error;
   NSString *fileContents_ =
       [NSString stringWithContentsOfFile:filePath
                                 encoding:NSUTF8StringEncoding
                                    error:&error];
-  
+
   NSString *fileDir = sandboxPath;
-  
+
   NSUInteger location = [filePath rangeOfString:[NSString stringWithFormat:@"/%@.js", fileName]].location;
   if (location > 0) {
     fileDir = [NSString stringWithFormat:@"%@/www/jxcore/",[filePath substringToIndex:location]];
   }
-  
+
   if (error) {
     NSLog(@"Error reading jxcore_cordova.js file: %@",
           error.localizedDescription);
@@ -369,19 +367,17 @@ static float delay = 0;
         "  node_module.addGlobalPath(process.userPath);\n"
         "};\n%@", documentsDirectory, fileDir, fileContents_];
 
-  JX_Initialize([fileDir UTF8String], callback);
-  JX_InitializeNewEngine();
-  JX_DefineExtension("callJXcoreNative", callJXcoreNative);
-  JX_DefineExtension("defineEventCB", defineEventCB);
-  JX_DefineMainFile([fileContents UTF8String]);
-  JX_StartEngine();
+  JS_SetProcessNative("callJXcoreNative", callJXcoreNative);
+  JS_SetProcessNative("defineEventCB", defineEventCB);
+  JS_DefineMainFile([fileContents UTF8String]);
+  JS_StartEngine([fileDir UTF8String]);
 
-  JX_LoopOnce();
+  JS_LoopOnce();
   [JXcore jxcoreLoop:[NSNumber numberWithInt:0]];
 }
 
 + (void)jxcoreLoop:(NSNumber *)n {
-  int result = JX_LoopOnce();
+  int result = JS_LoopOnce();
   float total_delay = delay + (result == 0 ? 0.05 : 0.01);
   
   if (useThreading)
@@ -407,7 +403,7 @@ static float delay = 0;
         NSTimeInterval waitInterval = delay + (result == 0 ? 0.05 : 0.01);
         [operationCheck waitUntilDate:[NSDate dateWithTimeIntervalSinceNow:waitInterval]];
         
-        result = JX_LoopOnce();
+        result = JS_LoopOnce();
       }
 
       if ([currentThread isCancelled]) {
@@ -450,7 +446,6 @@ static float delay = 0;
   [operationCheck unlock];
 }
 
-
 + (void)run:(void(^)())code_block withNativeCall:(NativeCall*)native
 {
   [queueCheck lock];
@@ -479,7 +474,7 @@ static float delay = 0;
 
 + (void) callEventCallbackNoThread:(NSString*)eventName withParams:(NSArray*)params isJSON:(BOOL) is_json {
   NSObject *cpp = [natives valueForKey:eventName];
-  JXValue *fnc;
+  JS_Value *fnc;
 
   if (cpp == nil) {
     cpp = [natives valueForKey:@"eventPing"];
@@ -490,83 +485,83 @@ static float delay = 0;
       return;
     }
   }
-  
+
   if (![cpp isKindOfClass:[CPPWrapper class]]) {
     NSLog(@"Error: Only Wrapped JXcore functions can be called from OBJ-C side");
     return;
   }
   fnc = [(CPPWrapper*)cpp getFunction];
-  
+
   unsigned nscount = (unsigned)[params count];
-  JXValue arr[2];
-  
+  JS_Value arr[2];
+
   const char* event_str = [eventName UTF8String];
   unsigned ln_event_str = (unsigned)[eventName length];
-  JX_New(&arr[0]);
-  JX_SetString(&arr[0], event_str, ln_event_str);
-  
-  if (!JX_CreateArrayObject(&arr[1])) {
+  JS_New(arr[0]);
+  JS_SetString(arr[0], event_str, ln_event_str);
+
+  if (!JS_CreateArrayObject(arr[1])) {
     NSLog(@"Wasnt able to create a new array object on JXcore callEventCallback. No memory left?");
     return;
   }
-  
+
   for(unsigned i=0; i < nscount; i++) {
     NSObject *objValue = [params objectAtIndex:i];
-    JXValue value;
-    JX_New(&value);
-    
+    JS_Value value;
+    JS_New(value);
+
     if ([objValue isKindOfClass:[NSString class]]) {
       NSString *strval = (NSString*)objValue;
-      
+
       const char* chval = [strval UTF8String];
       unsigned length = (unsigned)[strval lengthOfBytesUsingEncoding:NSUTF8StringEncoding];
       if (is_json)
-        JX_SetJSON(&value, chval, length);
+        JS_SetJSON(value, chval, length);
       else
-        JX_SetString(&value, chval, length);
+        JS_SetString(value, chval, length);
     } else if ([objValue isKindOfClass:[JXBoolean class]]) {
-      JX_SetBoolean(&value, [(JXBoolean*)objValue getBoolean] == TRUE);
+      JS_SetBoolean(&value, [(JXBoolean*)objValue getBoolean] == TRUE);
     } else if ([objValue isKindOfClass:[NSNumber class]]) {
       NSNumber *nmval = (NSNumber*)objValue;
       if (CFNumberIsFloatType((CFNumberRef)nmval) || CFNumberGetType((CFNumberRef)nmval) == kCFNumberDoubleType) {
-        JX_SetDouble(&value, [nmval doubleValue]);
+        JS_SetDouble(value, [nmval doubleValue]);
       } else {
-        JX_SetInt32(&value, [nmval intValue]);
+        JS_SetInt32(value, [nmval intValue]);
       }
     } else if ([objValue isKindOfClass:[NSData class]]) {
       NSData *data = (NSData*) objValue;
-      
+
       NSUInteger len = [data length];
       Byte byteData[len];
       memcpy(byteData, [data bytes], len);
-      
-      JX_SetBuffer(&value, (char*)byteData, (int32_t) len);
+
+      JS_SetBuffer(value, (char*)byteData, (int32_t) len);
     } else if ([objValue isKindOfClass:[JXJSON class]]) {
       NSString *strval = [(JXJSON*)objValue getString];
       const char* chval = [strval UTF8String];
       unsigned length = (unsigned)[strval lengthOfBytesUsingEncoding:NSUTF8StringEncoding];
-      JX_SetJSON(&value, chval, length);
+      JS_SetJSON(value, chval, length);
     } else {
-      JX_SetNull(&value);
+      JS_SetNull(value);
     }
-    
-    JX_SetIndexedProperty(&arr[1], i, &value);
-    JX_Free(&value);
+
+    JS_SetIndexedProperty(arr[1], i, value);
+    JS_Free(value);
   }
-  
-  JXValue ret_val;
-  JX_CallFunction(fnc, arr, 2, &ret_val);
-  
-  JX_Free(&ret_val);
-  
+
+  JS_Value ret_val;
+  JS_Value &fnc_val = *fnc;
+  JS_CallFunction(fnc_val, arr, 2, ret_val);
+  JS_Free(ret_val);
+
   for(unsigned i=0; i < 2; i++) {
-    JX_Free(&arr[i]);
+    JS_Free(arr[i]);
   }
 }
 
 + (void) callDirectMethod:(NSString*)eventName withParams:(NSArray*)params isJSON:(BOOL) is_json {
   NSObject *cpp = [natives valueForKey:eventName];
-  JXValue *fnc;
+  JS_Value *fnc;
   
   BOOL pingEvent = cpp == nil;
   if (pingEvent) {
@@ -582,26 +577,25 @@ static float delay = 0;
     NSLog(@"Error: Only Wrapped JXcore functions can be called from OBJ-C side");
     return;
   }
-  
+
   unsigned nscount = 0;
-  
+
   if (params != nil)
     nscount = (unsigned)[params count];
-  
-  
-  JXValue *arr = NULL;
+
+  JS_Value *arr = NULL;
   NSMutableData* mdata = nil;
-  
+
   if (nscount > 0)
   {
-    mdata = [NSMutableData dataWithLength:sizeof(JXValue) * nscount];
-    arr = (JXValue*) [mdata mutableBytes];
+    mdata = [NSMutableData dataWithLength:sizeof(JS_Value) * nscount];
+    arr = (JS_Value*) [mdata mutableBytes];
   }
   
   for(unsigned i=0; i < nscount; i++) {
     NSObject *objValue = [params objectAtIndex:i];
-    JXValue *value = arr+i;
-    JX_New(value);
+    JS_Value &value = arr[i];
+    JS_New(value);
     
     if ([objValue isKindOfClass:[NSString class]]) {
       NSString *strval = (NSString*)objValue;
@@ -609,43 +603,43 @@ static float delay = 0;
       const char* chval = [strval UTF8String];
       unsigned length = (unsigned)[strval lengthOfBytesUsingEncoding:NSUTF8StringEncoding];
       if (is_json)
-        JX_SetJSON(value, chval, length);
+        JS_SetJSON(value, chval, length);
       else
-        JX_SetString(value, chval, length);
+        JS_SetString(value, chval, length);
     } else if ([objValue isKindOfClass:[JXBoolean class]]) {
-      JX_SetBoolean(value, [(JXBoolean*)objValue getBoolean] == TRUE);
+      JS_SetBoolean(value, [(JXBoolean*)objValue getBoolean] == TRUE);
     } else if ([objValue isKindOfClass:[NSNumber class]]) {
       NSNumber *nmval = (NSNumber*)objValue;
       if (CFNumberIsFloatType((CFNumberRef)nmval) || CFNumberGetType((CFNumberRef)nmval) == kCFNumberDoubleType) {
-        JX_SetDouble(value, [nmval doubleValue]);
+        JS_SetDouble(value, [nmval doubleValue]);
       } else {
-        JX_SetInt32(value, [nmval intValue]);
+        JS_SetInt32(value, [nmval intValue]);
       }
     } else if ([objValue isKindOfClass:[NSData class]]) {
       NSData *data = (NSData*) objValue;
-      
+
       NSUInteger len = [data length];
       Byte byteData[len];
       memcpy(byteData, [data bytes], len);
-      
-      JX_SetBuffer(value, (char*)byteData, (int32_t) len);
+  
+      JS_SetBuffer(value, (char*)byteData, (int32_t) len);
     } else if ([objValue isKindOfClass:[JXJSON class]]) {
       NSString *strval = [(JXJSON*)objValue getString];
       const char* chval = [strval UTF8String];
       unsigned length = (unsigned)[strval lengthOfBytesUsingEncoding:NSUTF8StringEncoding];
-      JX_SetJSON(value, chval, length);
+      JS_SetJSON(value, chval, length);
     } else {
-      JX_SetNull(value);
+      JS_SetNull(value);
     }
   }
-  
-  JXValue ret_val;
-  JX_CallFunction(fnc, arr, nscount, &ret_val);
-  
-  JX_Free(&ret_val);
-  
+
+  JS_Value ret_val;
+  JS_Value &fnc_val = *fnc;
+  JS_CallFunction(fnc_val, arr, nscount, ret_val);
+  JS_Free(ret_val);
+
   for(int i=0; i<nscount; i++)
-    JX_Free(arr+i);
+    JS_Free(arr[i]);
 }
 
 + (void) callEventCallback:(NSString*)eventName_ withParams:(NSArray*)params_ isJSON:(BOOL) is_json {
@@ -703,16 +697,16 @@ static float delay = 0;
       
       [queueCheck unlock];
       
-      JXValue result;
+      JS_Value result;
       const char* str_script = [script UTF8String];
-      JX_Evaluate(str_script, "eval", &result);
-      JX_Free(&result);
+      JS_Evaluate(str_script, "eval", result);
+      JS_Free(result);
     } withEvalString:script_];
   } else {
-    JXValue result;
+    JS_Value result;
     const char* str_script = [script_ UTF8String];
-    JX_Evaluate(str_script, "eval", &result);
-    JX_Free(&result);
+    JS_Evaluate(str_script, "eval", result);
+    JS_Free(result);
   }
 }
 
